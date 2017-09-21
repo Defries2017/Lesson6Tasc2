3fbb0e40758e5e8106d3a592b75a9b2ef08d4591
Defries2017 <denis.fries75@gmail.com>
2017-09-21 20:42:47 +0300

    task finished

---
+++ src/Currency.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Currency {
+
+    // Расчет валют
+    // 1 - Перевод с курса в курс
+
+    public static int currency = 0;
+    public static double money = 0d;
+    public static String[] val = {"ГРН", "USD", "EUR"};
+
+    public static double USD = 26.08;
+    public static double EUR = 30.92;
+
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("( ГРН = 1; USD = 2; EUR = 3 )");
+
+        currency = scanner.nextInt();
+
+        System.out.print("Сумма ");
+        money = scanner.nextDouble();
+
+        if (currency == 1){
+            System.out.println("ГРН " + money);
+            System.out.println("USD " + (money / USD) );
+            System.out.println("EUR " + (money / EUR));
+        } else if (currency == 2){
+            System.out.println("ГРН " + (money / ( 1 / USD ) ) );
+            System.out.println("USD " + (money) );
+            System.out.println("EUR " + (money / (money / (money / ( 1 / USD ) ) * EUR)) );
+        } else if (currency == 3){
+            System.out.println("ГРН " + (money / ( 1 / EUR ) ));
+            System.out.println("USD " + ( money / (money / (money / ( 1 / EUR ) ) * USD)));
+            System.out.println("EUR " + money);
+        }
+    }
+}
